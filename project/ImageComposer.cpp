da0a5ead59c7e651f55d29162fce5c202dae9e26
HugoGellidaFac <hugo.gellida34@gmail.com>
2026-03-17 12:49:41 +0100

    implementation to continue

---
+++ project/ImageComposer.cpp
@@ -0,0 +1,16 @@
+#include "ImageComposer.h"
+#include <cmath>
+
+ImageBase ImageComposer::compose(const std::vector<unsigned char> &tilesData, int numTiles, std::vector<int> &compositionOrder){
+    int tileSize = std::sqrt(tilesData.size() / numTiles);
+    int gridSide = sqrt(compositionOrder.size());
+
+  ImageBase output(gridSide * tileSize, gridSide * tileSize, false);
+
+    for (int tileX = 0; tileX < gridSide; tileX++)
+        for (int tileY = 0; tileY < gridSide; tileY++)
+            for (int x = 0; x < tileSize; x++)
+                for (int y = 0; y < tileSize; y++)
+                    output[x + tileX * tileSize][y + tileY * tileSize] = tilesData[compositionOrder[tileX * gridSide + tileY] * tileSize + x * tileSize + y];
+  return output;
+}
